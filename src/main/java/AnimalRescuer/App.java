79957048976e2b643e517ebb236c8a1c0e1df90b
Roland <rolandattila@yahoo.com>
2019-09-10 15:25:57 +0300

    Exercised java project (Classes, objects, reference to object, instance of classes)

--- src/main/java/AnimalRescuer/App.java
+++ src/main/java/AnimalRescuer/App.java
@@ -1,13 +1,47 @@
 package AnimalRescuer;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        Animal dog = new Animal();
+        dog.name = "Rexi";
+        dog.age = 4;
+        dog.healthLevel = 10.0;
+        dog.hungryLevel = 10.0;
+        dog.moodLevel = 10.0;
+        dog.favouriteFood = "Cooked Chicken";
+        dog.preferredRecreationalActivityName = "Go to Camp";
+
+        Adopter adopter = new Adopter();
+        adopter.name = "Andrei";
+        adopter.budget = 450.50;
+
+        AnimalFood type = new AnimalFood();
+        type.name = "Porkchop";
+        type.price = 25.5;
+        type.quantity = 7;
+        type.expiringDate = "java.time.LocalDateTime";
+        type.stockAvailability = 5;
+
+        RecreationalActivity fun = new RecreationalActivity();
+        fun.name = "Mount Chillard";
+
+        Veterinarian veterinarian = new Veterinarian();
+        veterinarian.name = "Istvan";
+        veterinarian.specialization = "Veterinary";
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
